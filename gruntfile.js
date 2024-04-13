07fad3a99b0a04f65fd4113097030f65186fee52
joaoxmb <joaovictorcgdossantos@gmail.com>
2024-04-12 21:20:31 -0300

    Feat: Grunt finished set up

--- gruntfile.js
+++ gruntfile.js
@@ -8,8 +8,11 @@ module.exports = function (grunt) {
         }
       },
       production: {
+        options: {
+          compress: true,
+        },
         files: {
-          './dist/styles/main.css': './src/styles/main.less'
+          './dist/styles/main.min.css': './src/styles/main.less'
         }
       }
     },
@@ -17,13 +20,86 @@ module.exports = function (grunt) {
       less: {
         files: ['src/styles/**/*.less'],
         tasks: ['less:development']
+      },
+      html: {
+        files: ['src/index.html'],
+        tasks: ['replace:dev']
       }
-    }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'prebuild/index.html': 'src/index.html'
+        }
+      }
+    },
+    replace: {
+      dev: { 
+        options:{
+          patterns: [
+            {
+              match: 'CSS_SRC',
+              replacement: './styles/main.css'
+            },
+            {
+              match: 'JS_SRC',
+              replacement: '../src/scripts/main.js'
+            }
+          ],
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ['src/index.html'],
+            dest: 'dev/'
+          }
+        ]
+      },
+      dist: { 
+        options: {
+          patterns: [
+            {
+              match: 'CSS_SRC',
+              replacement: './styles/main.min.css'
+            },
+            {
+              match: 'JS_SRC',
+              replacement: './scripts/main.min.js'
+            }
+          ]
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ['prebuild/index.html'],
+            dest: 'dist/'
+          }
+        ]
+      }
+    },
+    uglify: {
+      target: {
+        files: {
+          './dist/scripts/main.min.js': './src/scripts/main.js'
+        }
+      }
+    },
+    clean: ['prebuild']
   })
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-replace');
 
   grunt.registerTask('default', ['watch']);
-  grunt.registerTask('build', ['less:production']);
+  grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'uglify', 'clean']);
 }
