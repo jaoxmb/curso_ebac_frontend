ce93bd6c3518cdf807aeff8998d140931d1f9adc
joaoxmb <joaovictorcgdossantos@gmail.com>
2024-07-16 22:47:12 -0300

    Fix: import characters images

--- src/scripts/characters.js
+++ src/scripts/characters.js
@@ -3,42 +3,56 @@ const resumeTitle = document.querySelector(".characters .title");
 const resumeText = document.querySelector(".characters .text");
 const resumeImage = document.querySelector(".characters__image");
 
+import violetPerson from "../assets/characters/violet.png";
+import violetCover from "../assets/characters/violet-cover.png";
+import gilbertPerson from "../assets/characters/gilbert.png";
+import gilbertCover from "../assets/characters/gilbert-cover.png";
+import claudiaPerson from "../assets/characters/claudia.png";
+import claudiaCover from "../assets/characters/claudia-cover.png";
+import benedictPerson from "../assets/characters/benedict.png";
+import benedictCover from "../assets/characters/benedict-cover.png";
+import cattleyaPerson from "../assets/characters/cattleya.png";
+import cattleyaCover from "../assets/characters/cattleya-cover.png";
+import ericaPerson from "../assets/characters/erica.png";
+import ericaCover from "../assets/characters/erica-cover.png";
+
+
 const characters = [
   {
     name: "Violet Evergarden",
     about: "Eu entrei na guerra como soldado quando era jovem. Porque eu só sabia lutar, eu ainda não entendo os sentimentos das pessoas e expressar meus sentimentos. Embora eu não pudesse entender o significado de uma palavra do oficial superior Gilbert no campo de batalha, encontrei o trabalho de Auto Memories Doll e comecei a procurar o significado dessa palavra.",
-    personImage: "src/assets/characters/violet.png",
-    coverImage: "src/assets/characters/violet-cover.png"
+    personImage: violetPerson,
+    coverImage: violetCover
   },
   {
     name: "Gilbert Bougainvillea",
     about: "Nascido para ser uma família aristocrática genérica que continua por gerações, ele se tornou Major do Exército. Durante a guerra, encontrei uma jovem violeta e ambos lutaram com os exércitos imperiais. É mais importante que qualquer coisa para Violet.",
-    personImage: "src/assets/characters/gilbert.png",
-    coverImage: "src/assets/characters/gilbert-cover.png"
+    personImage: gilbertPerson,
+    coverImage: gilbertCover
   },
   {
     name: "Claudia Hodgins",
     about: "Bom amigo dos tempos de escola militar de Gilbert. Era o Exército do Coronel, mas depois da guerra ele deixou a revista CH fora do exército. Estou cuidando do violeta a pedido de Gilbert. Devido ao personagem que ama a humanidade, ela também está se mostrando como um comerciante.",
-    personImage: "src/assets/characters/claudia.png",
-    coverImage: "src/assets/characters/claudia-cover.png"
+    personImage: claudiaPerson,
+    coverImage: claudiaCover
   },
   {
     name: "Benedict Blue",
     about: "Hodgins é um membro do antiquado, um dos funcionários que trabalha como entregador (carteiro) no posto do CH. Há compromisso em roupas, não vestindo uniformes de pessoal de entrega, mas vestindo uniformes.",
-    personImage: "src/assets/characters/benedict.png",
-    coverImage: "src/assets/characters/benedict-cover.png"
+    personImage: benedictPerson,
+    coverImage: benedictCover
   },
   {
     name: "Cattleya Baudelaire",
     about: "Um dos funcionários que trabalha na empresa postal CH. Há uma reputação de ser capaz de expressar sentimentos delicados que o cliente não pode falar em palavras, muitas vezes recebendo trabalho sob indicação. Com liberdade e generosidade, estou resumindo juniores incluindo violeta como idosos.",
-    personImage: "src/assets/characters/cattleya.png",
-    coverImage: "src/assets/characters/cattleya-cover.png"
+    personImage: cattleyaPerson,
+    coverImage:  cattleyaCover
   },
   {
     name: "Erica Brown",
     about: "CH Um dos funcionários que trabalhará como empresa postal. Iris senior, estou lutando para interagir com o cliente e não tenho certeza do meu trabalho.",
-    personImage: "src/assets/characters/erica.png",
-    coverImage: "src/assets/characters/erica-cover.png"
+    personImage: ericaPerson,
+    coverImage: ericaCover
   },
 ];
 
