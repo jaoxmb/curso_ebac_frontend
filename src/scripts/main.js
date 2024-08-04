30cd209052ed2c62f3db9b9eeae610f85a3a2e75
joaoxmb <joaovictorcgdossantos@gmail.com>
2024-08-04 16:13:51 -0300

    Created Counter logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
-console.log("hello world!")
+import { calcTime, formatedDate } from './utils';
+
+const handlerExpositor = () => {
+  const { days, hours, mins, secs } = calcTime();
+
+  document.querySelector("#expositor")
+    .textContent = `${days + "d"} ${hours + "h"} ${mins + "m"} ${secs + "s"}`;
+}
+const handlerExpositorDate = () => {
+  document.querySelector("#expositor_date")
+    .textContent = formatedDate();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setInterval(() => {
+    handlerExpositor();
+  }, 1000);
+
+  handlerExpositor();
+  handlerExpositorDate();
+})
